89f61c81e4baddf761a6305f6c239f7ddbdc9b23
Soumik Samanta <91832866+showMiko@users.noreply.github.com>
2025-11-01 18:22:18 +0530

    Models handling and rendering of URLS

--- learningDjango/basics/urls.py
+++ learningDjango/basics/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.all_basic_view, name='all_basic'),
+    path('item/<int:item_id>/', views.details_view, name='item_details'),
 ]
--- learningDjango/basics/views.py
+++ learningDjango/basics/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from .models import Item
+from django.shortcuts import render,get_list_or_404,get_object_or_404
+from .models import Item 
+
 # Create your views here.
 def all_basic_view(request):
     items=Item.objects.all()
     # context={'items':items}
     return render(request, 'basics/all_basics.html',{'items':items})
+
+def details_view(request, item_id):
+    item=get_object_or_404(Item, pk=item_id)
+    return render(request, 'basics/details.html', {'item':item})
